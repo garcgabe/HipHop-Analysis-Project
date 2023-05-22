5483a9aa09bed5e9b2b77f2d6eff034350a835aa
garcgabe <garciagabe0033@yahoo.com>
2023-05-21 22:33:44 -0400

    adding new py file

---
+++ HH_Spotify_Artists.py
@@ -0,0 +1,55 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import pandas as pd
+
+class spotWrapper():
+    def __init__(self):
+        client_id = 'accd0aa479164ddcbf1cbf822512b80b'
+        client_secret = '58bfc467435045e7b61c86fb03385729'
+        client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+        self.spot = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    def searchArtist(self, string):
+        return self.spot.search(q=string, type="artist")
+ 
+def readArtists():
+    artists = []
+    with open('/Users/garcgabe/Desktop/HipHop-Analysis-Project/artists.txt') as file:
+        name = 'x'
+        while(name != ''):
+            name = file.readline()
+            if name.strip() and 'stop' not in name:
+                artists.append(name.split("\n")[0])  
+            else:
+                break
+    return artists
+
+def fetch_artists():
+    spot = spotWrapper()
+    artists = readArtists()
+
+    # title of each column
+    columns = ["artist_name", "spotify_name", "artist_uri", "spotify_popularity", 
+            "spotify_followers","genres"]
+    # init empty lists to load then concat into DF
+    artist_name, spotify_name, artist_uri, genres, spotify_popularity, spotify_followers  = ([] for i in range (6))
+
+    for i in range(0,len(artists)):
+        # for each Name from Genius, search under artists and return data 
+        search_name = artists[i]
+        search_name_tree = spot.searchArtist(search_name)['artists']['items'][0]
+
+        artist_name.append(search_name)
+        spotify_name.append(search_name_tree["name"])
+        artist_uri.append(search_name_tree["uri"])
+        genres.append(search_name_tree["genres"])
+        spotify_popularity.append(search_name_tree["popularity"])
+        spotify_followers.append(search_name_tree["followers"]["total"])
+
+    return pd.concat([pd.Series(artist_name), pd.Series(spotify_name), pd.Series(artist_uri),
+                                        pd.Series(spotify_popularity), pd.Series(spotify_followers), pd.Series(genres)], 
+                                        axis=1, keys=columns)
+    
+if __name__=="__main__":
+    df = fetch_artists()
+    print(df)
+    
