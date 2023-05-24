35ca2f8f1978faabfffc4b16ba1cb76bacd15d23
garcgabe <garciagabe0033@yahoo.com>
2023-05-23 22:47:28 -0400

    ready for song fetch

--- HH_Spotify_Song.py
+++ HH_Spotify_Song.py
@@ -5,49 +5,50 @@ from fuzzywuzzy import fuzz
 
 class spotWrapper():
     def __init__(self):
+        # Hide these within venv variables
         client_id = 'accd0aa479164ddcbf1cbf822512b80b'
         client_secret = '58bfc467435045e7b61c86fb03385729'
         client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-        self.spot = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+        self.spot = spotipy.Spotify(client_credentials_manager=client_credentials_manager,
+                                    retries=10, status_retries=10)
 
     def getAlbumsTracks(self, id):
         return self.spot.album_tracks(id)
     def getAudioFeatures(self, song_uri):
         return self.spot.audio_features(song_uri)
 
-def clean_string(name):
-    cleaned = name.replace('\'','').replace('\n','').replace('’', '')
-    return cleaned
-    
-
 
 def call_data(all_albums, all_artists):
     spot = spotWrapper()
-
     print("getting songs for: " + str(len(all_albums)) + " albums.")
     song_uri, song_name, album_uri, artist_uris, artist_names, explicit, preview_url = ([] for i in range(7))
     columns = ["song_uri", "song_name", "album_uri", "artist_uris", "artist_names", "explicit", "preview_url"]
-    counter = 0
     for idx in all_albums:
         # search on album URI   
-        print("Album number "+ str(counter) )
-        counter+=1
-        if(counter > 100):
-            break
         album_data = spot.getAlbumsTracks(idx)["items"]
+        # temp arrays to hold multiple artist names and their uris
+        temp_artists, temp_artist_uris = ([] for x in range(2))
         number_of_artists = len(album_data[0]["artists"])
+        # search for cases where there are multiple artists and artist_uris
+        # need to parse and add as a list instead of a single value
         for num in range(0,number_of_artists):
             artist_name_in_list = album_data[0]["artists"][num]["name"]
             try:
                 artist_uri_in_list = all_artists.loc[all_artists["spotify_name"] == artist_name_in_list, "artist_uri"].iloc[0]
             except:
                 artist_uri_in_list = "0"
+            temp_artists.append(artist_name_in_list)
+            temp_artist_uris.append(artist_uri_in_list)
+        # join these lists into one variables to append to list for DF addition
+        # each song within this loop iteration will share these 2 variables since same album
+        album_artists = ", ".join(temp_artists)
+        album_artist_uris = ", ".join(temp_artist_uris)
 
-
+        # populate lists with song data
         for song_num in range(0,len(album_data)):  
             album_uri.append(idx)  
-            artist_uris.append(artist_uri_in_list)
-            artist_names.append(artist_name_in_list)
+            artist_uris.append(album_artist_uris)
+            artist_names.append(album_artists)
             song_uri.append(album_data[song_num]["uri"])
             name = album_data[song_num]["name"]
             song_name.append(name)
@@ -55,15 +56,12 @@ def call_data(all_albums, all_artists):
             explicit.append(album_data[song_num]["explicit"])
             preview_url.append(album_data[song_num]["preview_url"])
 
-
-
-
+    # concatenate the lists 
     song_df = pd.concat([pd.Series(song_uri), pd.Series(song_name), pd.Series(album_uri),pd.Series(artist_uris),
                                         pd.Series(artist_names), pd.Series(explicit), pd.Series(preview_url)], 
                                         axis=1, keys=columns)
-    song_df.to_excel("interm_songs.xlsx")
+    # pass DF through to get song metrics based on unique song_uri
     return add_song_metrics(song_df)
-
 
 
 
@@ -72,9 +70,14 @@ def add_song_metrics(df):
 
     spot = spotWrapper()
     for i in range(0, len(df)):
-        song_uri = df.loc[i]["song_uri"]
-        spot_audio = spot.getAudioFeatures(song_uri)
+        # simple logging to check progress
+        if( i%250 == 0):
+            print("Song Number: " + str(i)+"/"+str(len(df)) )
+        # get song_uri to uniquely identify song and search for features
+        uri = df.loc[i]["song_uri"]
+        spot_audio = spot.getAudioFeatures(uri)
 
+        # add song data to lists of metrics
         dance.append(spot_audio[0]['danceability'])
         energy.append(spot_audio[0]['energy'])
         loudness.append(spot_audio[0]['loudness'])
@@ -83,7 +86,7 @@ def add_song_metrics(df):
         instru.append(spot_audio[0]['instrumentalness'])
         speech.append(spot_audio[0]['speechiness'])
 
-    # Add arrays of song characteristics into main DataFrame
+    # Add arrays of song characteristics into main DF and return
     df['dance'] = dance
     df['energy'] = energy
     df['loudness'] = loudness
@@ -95,13 +98,20 @@ def add_song_metrics(df):
 
 if __name__ == "__main__":
     #CHANGE to read from Dim Artist and Dim Albums
-    album_df = pd.read_excel("SpotifyAlbums.xlsx")
-    artist_df = pd.read_excel("SpotifyArtists.xlsx")
 
-    spot_artist_names = artist_df[["spotify_name", "artist_uri"]]
+    # get album_uris to search for songs
+    album_df = pd.read_excel("SpotifyAlbums.xlsx")
     spot_album_uris = album_df["album_uri"]
+    # get artist names and uris to add to song data
+    artist_df = pd.read_excel("SpotifyArtists.xlsx")
+    spot_artist_names = artist_df[["spotify_name", "artist_uri"]]
+
+    #actual fetch of songs into dataframe
     spot_songs = call_data(spot_album_uris, spot_artist_names)
 
-    print(spot_songs.head() )
+    #persist dataframe in excel
+    spot_songs.to_excel("interm_songs.xlsx")
+
+    print(spot_songs)
     #spot_songs.to_excel("SpotifySongs.xlsx")
 
