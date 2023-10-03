0e496f21f5135e8ea2d84f8b58423a742ec090df
garcgabe <garciagabe0033@yahoo.com>
2023-10-03 17:23:39 -0400

    songs uploaded to db

--- HH_Spotify_Song.py
+++ HH_Spotify_Song.py
@@ -8,16 +8,14 @@ import sys, json
 db = Postgres()
 spot = SpotifyWrapper()
 
-def call_data(all_albums, all_artists):
-    seen_artists = {key: False for key in all_artists}
-    print(all_albums)
+def call_data(all_albums):
 
     print("Fetching songs for: " + str(len(all_albums)) + " albums.")
-    song_uri, song_name, album_uri, artist_names, explicit, preview_url = ([] for i in range(6))
+    song_uri, song_name, album_uri, artist_names, explicit, preview_url = ([] for _ in range(6))
     columns = ["song_uri", "song_name", "album_uri", "artist_names", "explicit", "preview_url"]
     for counter in range(0,len(all_albums)):
         uri = all_albums.iloc[counter]["album_uri"]
-        print(counter, uri)
+        #print(counter, uri)
         if(counter%25 == 0):
             print("Album #: "+str(counter))
         
@@ -31,53 +29,55 @@ def call_data(all_albums, all_artists):
         # need to parse and add as a list instead of a single value
         for num in range(0,number_of_artists):
             artist_name_in_list = album_data[0]["artists"][num]["name"]
-            seen_artists[artist_name_in_list] = True
             temp_artists.append(artist_name_in_list.replace(",", ""))
         # join these lists into one variables to append to list for DF addition
         # each song within this loop iteration will share these 2 variables since same album
         album_artists = "-".join(temp_artists)
 
         # populate lists with song data
-        for song_num in range(0,len(album_data)):  
-            song_uri.append(album_data[song_num]["uri"])
+        for song_num in range(0,len(album_data)): 
+            temp_song_uri =  album_data[song_num]["uri"]
+            song_uri.append(temp_song_uri)
 
             name = album_data[song_num]["name"]
-            song_name.append(name.replace(",", ""))
+            temp_song_name = name.replace(",", "")
+            song_name.append(temp_song_name)
 
             album_uri.append(uri)  
             artist_names.append(album_artists)
 
             #print(f"Song name: {name} by {artist_name_in_list}")
-            explicit.append(album_data[song_num]["explicit"])
-            preview_url.append(album_data[song_num]["preview_url"])
+            temp_explicit = album_data[song_num]["explicit"]
+            explicit.append(temp_explicit)
+
+            temp_preview_url = album_data[song_num]["preview_url"]
+            preview_url.append(temp_preview_url)
+
+            # commit to DB
+            ## NOTE: this data is static, therefore on conflicts we do nothing to the existing rows
+            ## on conflict do nothing - album already accounted for by prior artist
+
+            db.execute_query(f"""
+                INSERT INTO songs (song_uri, song_name, album_uri, artist_names, explicit, preview_url)
+                    VALUES (%s, %s, %s, %s, %s, %s)
+                    ON CONFLICT (song_uri)
+                    DO NOTHING
+                """, (temp_song_uri, temp_song_name, uri, album_artists, temp_explicit, temp_preview_url))
 
     # concatenate the lists 
-    song_df = pd.concat([pd.Series(song_uri), pd.Series(song_name), pd.Series(album_uri),
+    return pd.concat([pd.Series(song_uri), pd.Series(song_name), pd.Series(album_uri),
                                         pd.Series(artist_names), pd.Series(explicit), pd.Series(preview_url)], 
                                         axis=1, keys=columns)
-    print(song_df.sort_values("song_name"))
-    # pass DF through to get song metrics based on unique song_uri
-    song_df = song_df.sort_values("explicit", ascending=False)
-    return song_df.drop_duplicates(subset="song_uri", keep='first')
 
 
 if __name__ == "__main__":
-    #CHANGE to read from Dim Artist and Dim Albums
+    # Read from album table in DB
     spot_album_uris = pd.DataFrame(db.fetch_data(f"""
             SELECT album_uri FROM albums;
             """), columns = ["album_uri"]
     )
-    spot_album_uris = spot_album_uris[:10]
 
-    # get artist names and uris to add to song data
-    spot_artist_names = pd.DataFrame(db.fetch_data(f"""
-            SELECT artist_uri, spotify_name FROM artists;
-            """), columns = ("artist_uri", "spotify_name")
-    )
-
-    #actual fetch of songs into dataframe
-    spot_songs = call_data(spot_album_uris, spot_artist_names)
+    #actual fetch of songs into DB
+    spot_songs = call_data(spot_album_uris)
     
-    print(spot_songs.sort_values("song_name"))
-    # spot_songs.to_csv("/Users/garcgabe/Desktop/HipHop-Analysis-Project/data/SpotifySongs")
 
--- README.md
+++ README.md
@@ -7,3 +7,11 @@ query to replicate:
         select * from albums where album_name in (select album_name from albums
         group by 1
         having count(album_name) = 2)
+
+- you can just dump all the songs into SQL for now. if there happen to be duplicates based on explicit/clean,
+we can add SQL logic to clean it up. just dump it all there for now and use SQL to filter and slice
+
+- there are many song duplicates
+---- they may be same song name, diff artist, same song name, same artist, diff album, and these have different song_uris, so may have to create some logic that aggregates the songs if they're on different albums.
+
+-- idk we'll see
