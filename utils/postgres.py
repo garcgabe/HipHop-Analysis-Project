f7be9c34b052c136c58e510ef371e2da73ebbbd5
garcgabe <garciagabe0033@yahoo.com>
2023-12-18 19:46:43 -0500

    changing postgres info to access from streamlit

--- utils/postgres.py
+++ utils/postgres.py
@@ -1,11 +1,11 @@
 import psycopg2 as pg
-from utils.env import host, database, user, password, port
+#from utils.env import host, database, user, password, port
 
 class Postgres():
     # holds two attributes:
     #   connection to the database
     #   cursor, which is used to execute queries and close itself
-    def __init__(self):
+    def __init__(self, host, database, user, password, port):
         self.conn = pg.connect(
             host=host,
             database=database,
