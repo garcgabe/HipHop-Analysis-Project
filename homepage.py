60d31b34de8cb70e911e2502528ea147b3bd8ab5
garcgabe <garciagabe0033@yahoo.com>
2024-01-05 12:18:55 -0500

    metric card design

--- homepage.py
+++ homepage.py
@@ -57,8 +57,8 @@ st.divider()
 # column layout
 col1, col2 = st.columns([1,1])  # Adjust the column widths as needed
 selection = result.loc[result['artist_name'] == selected_name]
-col1.metric('popularity!', f"{int(selection['popularity'][0]):,}")
-col2.metric('followers!', selection['followers'][0])
+col1.metric('popularity!', selection['popularity'][0])
+col2.metric('followers!', f"{int(selection['followers'][0]):,}")
 st.metric('genres!', \
     ", ".join(genre for genre in selection['genres'][0].split('-')),\
         )
