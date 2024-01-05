7b58bb6ac3a85eb4db105432d73c0753b0b1fc11
garcgabe <garciagabe0033@yahoo.com>
2024-01-05 11:46:34 -0500

    bringing in extra, removing supabase_connection module

--- homepage.py
+++ homepage.py
@@ -2,9 +2,7 @@ import streamlit as st
 import os
 import pandas as pd
 from supabase import create_client
-
-from st_supabase_connection import SupabaseConnection
-
+from streamlit_extras.metric_cards import style_metric_cards
 
 url = st.secrets["supabase_url"].SUPABASE_URL
 key = st.secrets["supabase_key"].SUPABASE_KEY
@@ -58,42 +56,14 @@ st.markdown(
 # column layout
 col1, col2 = st.columns([1,1])  # Adjust the column widths as needed
 selection = result.loc[result['artist_name'] == selected_name]
-# col1.metric('popularity!', selection['popularity'][0])
-# col2.metric('followers!', selection['followers'][0])
-# st.metric('genres!', \
-#     ", ".join(genre for genre in selection['genres'][0].split('-')),\
-#         )
-col1.markdown(
-    '<div class="center"><h2>popularity!</h2><p>{}</p></div>'.format(selection['popularity'][0]),
-    unsafe_allow_html=True
-)
+col1.metric('popularity!', selection['popularity'][0])
+col2.metric('followers!', selection['followers'][0])
+st.metric('genres!', \
+    ", ".join(genre for genre in selection['genres'][0].split('-')),\
+        )
 
-col2.markdown(
-    '<div class="center"><h2>followers!</h2><p>{}</p></div>'.format(selection['followers'][0]),
-    unsafe_allow_html=True
-)
 
-st.markdown(
-    '<div class="center"><h2>genres!</h2><p>{}</p></div>'.format(", ".join(genre for genre in selection['genres'][0].split('-'))),
-    unsafe_allow_html=True
-)
 
-st.markdown(
-    """
-    <div style="display: flex; justify-content: space-between;">
-        <div style="width: 33%; text-align: left;">
-            <h3>Top Left</h3>
-        </div>
-        <div style="width: 33%; text-align: right;">
-            <h3>Top Right</h3>
-        </div>
-    </div>
-    <div style="width: 100%; text-align: center;">
-        <h3>Centered</h3>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
 
 # col2.dataframe(result.drop('spotify_name', axis=1),\
 #     # column_config={
