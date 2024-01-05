9015de3278851856c2cfcce4ba69f599996e093b
garcgabe <garciagabe0033@yahoo.com>
2024-01-05 10:36:20 -0500

    displaying selected artist data

--- homepage.py
+++ homepage.py
@@ -19,11 +19,8 @@ response = supabase.table("artists")\
 artists = [x.get('artist_name') for x in response.data]
 
 # SELECT ARTIST FROM FRONTEND
-
 selected_name = st.selectbox("select an artist", \
     options=artists)
-
-
 
 # querying all artists and info
 filter_response = supabase.table("artists")\
@@ -31,13 +28,16 @@ filter_response = supabase.table("artists")\
     .eq('artist_name', f'{selected_name}')\
     .execute()
 
+# convert all data to DF; then 
 result = pd.DataFrame(filter_response.data)
-result_image = result[['artist_name', 'images']]
 
-selected_artist_image = result_image.loc[result_image['artist_name'] == selected_name]['images'][0]
+# identify image and uri we will use going ahead
+selected_artist_image = result.loc[result['artist_name'] == selected_name]['images'][0]
 selected_artist_uri = result.loc[result['artist_name'] == selected_name]['artist_uri'][0]
 
+# remove uri and image from data table
 result = result.drop(['artist_uri','images'], axis=1)
+
 # column layout
 col1, col2 = st.columns(2)  # Adjust the column widths as needed
 col1.image(selected_artist_image, width=200)
@@ -47,14 +47,15 @@ st.dataframe(result,\
     # },
     )
 
-
-# gets artist names - delimited by 
-# albums = conn.query("""
-#             SELECT album_uri, artist_uris
-# """)
+# TODO: bring in album metrics
+#        - number of albums
+#
+#
+# 
+#
+#
 
 ## query to filter by artists
-# selected_artist_uri = None #add
 
 # filtered_album_uris = conn.query(f"""
 #             SELECT album_uri FROM albums
@@ -63,13 +64,5 @@ st.dataframe(result,\
 
 
 
-#st.write(artists)
-
-st.sidebar.markdown("rap analytics")
-
 st.title("welcome to the rap analytics page")
 
-
-
-
-#st.image("")
