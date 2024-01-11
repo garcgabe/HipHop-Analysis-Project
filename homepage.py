a876ec5d52e3e706ba8037f128806938bbaa346c
garcgabe <garciagabe0033@yahoo.com>
2024-01-11 14:14:37 -0500

    moved pareto card to new file

--- homepage.py
+++ homepage.py
@@ -2,25 +2,9 @@ import streamlit as st
 import os
 import pandas as pd
 from utils import queries
+from utils import pareto_card
 from supabase import create_client
 from streamlit_extras.metric_cards import style_metric_cards
-
-##
-def _generate_genre_html(genres):
-  genre_html = "<h5>"
-  number_genres = len(selected_artist_genres)
-  if number_genres <= 2:
-      genre_html += " --- ".join(selected_artist_genres)
-  elif(number_genres <=6):
-      for i in range(0, number_genres, 2):
-          genre_html += " --- ".join(selected_artist_genres[i:i+2])
-          genre_html += "<br>"
-  else:
-      for i in range(0, 6, 2):
-        genre_html += " --- ".join(selected_artist_genres[i:i+2])
-        genre_html += "<br>"
-  genre_html += "</h5>"
-  return genre_html
 
 ###################################################
 # get all artist options from DB
@@ -40,125 +24,10 @@ selected_artist_genres = result.loc[result['artist_name'] == selected_name]['gen
 # remove uri and image from data table
 result = result.drop(['artist_uri','images'], axis=1)
 
-genre_html = _generate_genre_html(selected_artist_genres)
+pareto_card.generate(artist, selected_artist_genres, selected_artist_image)
 
-topsongs = queries._get_top_songs(selected_name, 5)
-popularity_distribution = queries._get_distribution(selected_name, 'popularity')
-dance_distribution = queries._get_distribution(selected_name, 'danceability')
-emotion_distribution = queries._get_distribution(selected_name, 'valence')
-energy_distribution = queries._get_distribution(selected_name, 'energy')    
-
-st.markdown("""
-<style>
-  body { font-family: Helvetica, sans-serif ;
-  color: white;
-   }
-  .container { display: flex; flex-wrap: wrap;
-    max-width: 700px; 
-    margin: auto;}
-  .card { width: 300px; height: 500px; border: 2px solid #AAA; 
-   background-image: linear-gradient(to bottom right, #133832, #552506);
-    border-image: linear-gradient(to right, #F1E1A4, #FFFFFF);
-    border-image-slice: 1;
-    background-radius: 15px;
-    flex:None;
-    margin: 10px;
-    }
-  .card-header { text-align: center; justify-content: center; }
-  .card-content { text-align: center; justify-content: center; margin-bottom: 10px; }
-  .artist_pic { width:275px; height:275px; border-radius:50%; justify-content: center;}
-  .front_info { font-family: Helvetica, sans-serif; word-wrap: break-word; max-width: 350px; margin: 0 auto;}
-  h4{margin-top: 0; margin-bottom: 0; bottom: 0}
-  p{margin-top: 0; margin-bottom: 0; }
-  .top-songs {
-    display: flex;
-    flex-direction: column;
-  }
-  .song {
-    display: flex;
-    justify-content: space-between;
-    padding-left: 10px;
-  }
-  .title {
-    text-align: left;
-  }
-  .popularity {
-    text-align: right;
-    padding-right: 10px;
-  }
-  .metrics { text-align: bottom; }
-  .metric { margin-bottom: 5px; }
-
-  /* Responsive design */
-  @media (max-width: 610px) {
-    .container {
-      flex-direction: column;
-      align-items: center; /* This will center the cards when stacked */
-    }
-  }
-</style>
-""", unsafe_allow_html=True)
-
-st.markdown(f"""
-<body>
-<div class="container"> 
-<div class="card">
-  <div class="card-header">
-    <h3>{selected_name}</h3>
-  </div>
-  <div class="card-content">
-    <img src="{selected_artist_image}" class = "artist_pic">
-    <div class="front_info">
-      {genre_html}
-      <h6>◍ - ◍ - ◍ - ◍ - ◍ - ◍ - ◍ - ◍ - ◍ - ◍ - ◍ - ◍</h6>
-      <h4>Pareto Score:   30%</h4>
-    </div>
-  </div>
-</div>
-
-<div class="card">
-    <div class="card-header">
-        <h3>Most Popular</h3>
-    </div>
-    <div class="card-content">
-      <div class="top-songs">
-        <div class="song">
-          <span class="title">{topsongs[0][0]}</span>
-          <span class="popularity">{topsongs[0][1]}</span>
-        </div>
-        <div class="song">
-          <span class="title">{topsongs[1][0]}</span>
-          <span class="popularity">{topsongs[1][1]}</span>
-        </div>
-        <div class="song">
-          <span class="title">{topsongs[2][0]}</span>
-          <span class="popularity">{topsongs[2][1]}</span>
-        </div>
-        <div class="song">
-          <span class="title">{topsongs[3][0]}</span>
-          <span class="popularity">{topsongs[3][1]}</span>
-        </div>
-        <div class="song">
-          <span class="title">{topsongs[4][0]}</span>
-          <span class="popularity">{topsongs[4][1]}</span>
-        </div>
-      </div>
-      <h6>◍ - ◍ - ◍ - ◍ - ◍ - ◍ - ◍ - ◍ - ◍ - ◍ - ◍ - ◍</h6 >
-      <div class="card-header">
-        <h3>Min | Mean | Max</h3>
-      </div>
-      <div class="metrics">
-        <div class="metric"><strong>Popularity: </strong>{" - ".join(str(round(x)) for x in popularity_distribution)}</div>
-        <div class="metric"><strong>Energy: </strong>{" - ".join(str(round(x,2)) for x in energy_distribution)}</div>
-        <div class="metric"><strong>Danceability: </strong>{" - ".join(str(round(x,2)) for x in dance_distribution)}</div>
-        <div class="metric"><strong>Emotion: </strong>{" - ".join(str(round(x,2)) for x in emotion_distribution)}</div>
-      </div>
-    </div>
-</div>
-</div>
-
-</body>
-""", unsafe_allow_html=True)
+st.markdown(pareto_card[0], unsafe_allow_html=True)
+st.markdown(pareto_card[1], unsafe_allow_html=True)
 
 # # centered artist image
 # st.markdown(
---
+++ utils/pareto_card.py
@@ -0,0 +1,123 @@
+import queries
+
+
+def _generate_genre_html(genres):
+  genre_html = "<h5>"
+  number_genres = len(selected_artist_genres)
+  if number_genres <= 2:
+      genre_html += " --- ".join(selected_artist_genres)
+  elif(number_genres <=6):
+      for i in range(0, number_genres, 2):
+          genre_html += " --- ".join(selected_artist_genres[i:i+2])
+          genre_html += "<br>"
+  else:
+      for i in range(0, 6, 2):
+        genre_html += " --- ".join(selected_artist_genres[i:i+2])
+        genre_html += "<br>"
+  genre_html += "</h5>"
+  return genre_html
+
+def generate(artist, genres, image):
+    genre_html = _generate_genre_html(genres)
+
+    topsongs = queries._get_top_songs(artist, 5)
+    popularity_distribution = queries._get_distribution(artist, 'popularity')
+    dance_distribution = queries._get_distribution(artist, 'danceability')
+    emotion_distribution = queries._get_distribution(artist,'valence')
+    energy_distribution = queries._get_distribution(artist, 'energy')    
+    style_html = """
+        <style>
+          body { font-family: Helvetica, sans-serif; color: white; }
+          .container { display: flex; flex-wrap: wrap; max-width: 700px; margin: auto;}
+          .card { width: 300px; height: 500px; border: 2px solid #AAA; 
+            background-image: linear-gradient(to bottom right, #133832, #552506);
+            background-radius: 15px;
+            border-image: linear-gradient(to right, #F1E1A4, #FFFFFF);
+            border-image-slice: 1;
+            flex:None; margin: 10px;
+            }
+          .card-header { text-align: center; justify-content: center; }
+          .card-content { text-align: center; justify-content: center; margin-bottom: 10px; }
+          .artist_pic { width:275px; height:275px; border-radius:50%; justify-content: center;}
+          .front_info { font-family: Helvetica, sans-serif; word-wrap: break-word; max-width: 350px; margin: 0 auto;}
+          h4{margin-top: 0; margin-bottom: 0; bottom: 0}
+          p{margin-top: 0; margin-bottom: 0; }
+          .top-songs { display: flex; flex-direction: column;}
+          .song { display: flex; justify-content: space-between; padding-left: 10px;}
+          .title {text-align: left; }
+          .popularity { text-align: right; padding-right: 10px; }
+          .metrics { text-align: bottom; }
+          .metric { margin-bottom: 5px; }
+
+          /* Responsive design */
+          @media (max-width: 610px) {
+            .container {
+              flex-direction: column;
+              align-items: center; 
+            }
+          }
+        </style>
+      """
+
+    content_html = f"""
+        <body>
+        <div class="container"> 
+        <div class="card">
+          <div class="card-header">
+            <h3>{artist}</h3>
+          </div>
+          <div class="card-content">
+            <img src="{image}" class = "artist_pic">
+            <div class="front_info">
+              {genre_html}
+              <h6>◍ - ◍ - ◍ - ◍ - ◍ - ◍ - ◍ - ◍ - ◍ - ◍ - ◍ - ◍</h6>
+              <h4>Pareto Score:   30%</h4>
+            </div>
+          </div>
+        </div>
+
+        <div class="card">
+            <div class="card-header">
+                <h3>Most Popular</h3>
+            </div>
+            <div class="card-content">
+              <div class="top-songs">
+                <div class="song">
+                  <span class="title">{topsongs[0][0]}</span>
+                  <span class="popularity">{topsongs[0][1]}</span>
+                </div>
+                <div class="song">
+                  <span class="title">{topsongs[1][0]}</span>
+                  <span class="popularity">{topsongs[1][1]}</span>
+                </div>
+                <div class="song">
+                  <span class="title">{topsongs[2][0]}</span>
+                  <span class="popularity">{topsongs[2][1]}</span>
+                </div>
+                <div class="song">
+                  <span class="title">{topsongs[3][0]}</span>
+                  <span class="popularity">{topsongs[3][1]}</span>
+                </div>
+                <div class="song">
+                  <span class="title">{topsongs[4][0]}</span>
+                  <span class="popularity">{topsongs[4][1]}</span>
+                </div>
+              </div>
+              <h6>◍ - ◍ - ◍ - ◍ - ◍ - ◍ - ◍ - ◍ - ◍ - ◍ - ◍ - ◍</h6 >
+              <div class="card-header">
+                <h3>Min | Mean | Max</h3>
+              </div>
+              <div class="metrics">
+                <div class="metric"><strong>Popularity: </strong>{" - ".join(str(round(x)) for x in popularity_distribution)}</div>
+                <div class="metric"><strong>Energy: </strong>{" - ".join(str(round(x,2)) for x in energy_distribution)}</div>
+                <div class="metric"><strong>Danceability: </strong>{" - ".join(str(round(x,2)) for x in dance_distribution)}</div>
+                <div class="metric"><strong>Emotion: </strong>{" - ".join(str(round(x,2)) for x in emotion_distribution)}</div>
+              </div>
+            </div>
+        </div>
+        </div>
+
+        </body>
+        """
+    return (style_html, content_html)
+      
