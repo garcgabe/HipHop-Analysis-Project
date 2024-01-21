d4f8e3c53c60faa7aefc86fa0b7bc5dac0f51a4e
garcgabe <garciagabe0033@yahoo.com>
2024-01-20 19:38:35 -0500

    noncard view

--- homepage.py
+++ homepage.py
@@ -46,7 +46,7 @@ st.markdown("""
                 border-radius: 2px;
                 border-left: 0.5rem solid #9AD8E1;}
     .metric_label { text-align: left; font-weight: bold; color: #9AD8E1; }
-    .metric_value { text-align: right; }
+    .metric_value { text-align: right; padding: 5px; color: white; }
 
     </style>
 """, unsafe_allow_html=True)
