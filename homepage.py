52cdfad271131ec764c3d0dfe2736821daf2679a
garcgabe <garciagabe0033@yahoo.com>
2024-01-21 17:08:46 -0500

    noncard view

--- homepage.py
+++ homepage.py
@@ -50,7 +50,7 @@ st.markdown("""
     .title {text-align: left; }
     .popularity { text-align: right; padding-right: 20px; }
     .metrics { text-align: bottom; }
-    #.metric { padding-left: 20px; padding-right: 20px; margin-bottom: 5px; }
+    .metric { padding-left: 20px; padding-right: 20px; margin-bottom: 5px; }
     .metric_title { padding-left: 20px; padding-right: 20px; margin-bottom: 5px;}
     h6 { color: #9AD8E1; font-size: 20px; font-weight: bold; }
 
