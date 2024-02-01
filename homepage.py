5636a1364a928fd35996f99355aa0c054c6e13d9
garcgabe <garciagabe0033@yahoo.com>
2024-02-01 16:52:11 -0500

    added album dropdown for songs tab

--- homepage.py
+++ homepage.py
@@ -167,6 +167,8 @@ with albums_tab:
     st.dataframe(album_averages[["album name", "popularity", "danceability", "energy", "valence"]], use_container_width=True, hide_index=True)
 
 with songs_tab:
+    albums = queries._get_albums(selected_artist_uri)
+    selected_album = st.selectbox("select an album to view", options=albums)
     st.dataframe(all_songs_statistics, use_container_width=True, hide_index=True)
 
 #
