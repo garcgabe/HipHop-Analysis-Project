3ce7cc057e64bb8d21e7c03393a41b82bb699606
gabe garcia <gabegarcia@gabes-MacBook-Pro.local>
2024-03-15 00:23:44 -0400

    access token acquired

--- scripts/artists_fetch.py
+++ scripts/artists_fetch.py
@@ -1,4 +1,34 @@
+# tools
+import requests, base64
+
+# resources
 from utils.env import SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET
 
+def _post_credentials():
+    # Encode the client ID and client secret
+    credentials = f"{SPOTIFY_CLIENT_ID}:{SPOTIFY_CLIENT_SECRET}"
+    credentials_bytes = credentials.encode('ascii')
+    base64_bytes = base64.b64encode(credentials_bytes)
+    base64_credentials = base64_bytes.decode('ascii')
+    
+    response = requests.post('https://accounts.spotify.com/api/token',
+                             headers = {
+                                 'Authorization': f"Basic {base64_credentials}",
+                                 'Content_Type': "application/x-www-form-urlencoded"
+                             },
+                             data = {'grant_type': 'client_credentials'}
+                             )
+    if response.status_code == 200:
+        token = response.json()['access_token']
+        print(f"Access Token received as: {token}")
+        return token
+    else:
+        print(f"Failed request: {response.status_code}")
+
+
 if __name__=="__main__":
-    print(SPOTIFY_CLIENT_ID[:10])
+    access_token = _post_credentials()
+
+
+
+
