adb180fc3408d0765576ad89d60b8fa03ffc1d14
gabe garcia <garciagabe0033@yahoo.com>
2024-03-25 08:42:50 -0400

    new data model final

--- creation.sql
+++ creation.sql
@@ -1,11 +1,11 @@
 -- create artists table
 CREATE TABLE artists(
     artist_uri varchar(255) primary key,
-    artist_name varchar(255),
+    artist_name varchar(255) not null,
     popularity integer,
     followers integer,
     genres varchar(255),
-    images varchar(255)
+    images varchar(500)
 );
 
 -- create albums table
@@ -14,33 +14,33 @@ CREATE TABLE albums(
     album_name varchar(255) not null,
     release_date date,
     total_tracks integer,
-    images varchar(255)
+    images varchar(500)
 );
 
 -- create album-artists relations table
 CREATE TABLE album_artists(
-    album_uri varchar(255),
-    artist_uri varchar(255),
+    album_uri varchar(255) not null,
+    artist_uri varchar(255) not null,
     PRIMARY KEY (album_uri, artist_uri),
     FOREIGN KEY (album_uri) REFERENCES albums (album_uri) ON DELETE CASCADE,
     FOREIGN KEY (artist_uri) REFERENCES artists (artist_uri) ON DELETE CASCADE
-)
+);
 
 -- create songs table
 CREATE TABLE songs(
     song_uri varchar(255) primary key,
     song_name varchar(255) not null,
-    album_uri varchar(255),
+    album_uri varchar(255) not null,
     explicit boolean,
-    preview_url varchar(255),
+    preview_url varchar(500),
     FOREIGN KEY (album_uri) REFERENCES albums (album_uri) ON DELETE CASCADE
 );
 
 -- create song metrics table
 CREATE TABLE metrics(
     song_uri varchar(255) primary key,
-    song_name varchar(255),
-    duration int,
+    song_name varchar(255) not null,
+    duration int not null,
     popularity int,
     danceability decimal(4,3),
     energy decimal(4,3),
@@ -49,7 +49,7 @@ CREATE TABLE metrics(
     tempo decimal(5,1),
     instrumentalness decimal(4,4),
     speechiness decimal(4,3),
-    album_uri varchar(255),
-    FOREIGN KEY (song_uri) REFERENCES songs (song_uri) ON DELETE CASCADE
+    album_uri varchar(255) not null ,
+    FOREIGN KEY (song_uri) REFERENCES songs (song_uri) ON DELETE CASCADE,
     FOREIGN KEY (album_uri) REFERENCES albums (album_uri) ON DELETE CASCADE
 );
